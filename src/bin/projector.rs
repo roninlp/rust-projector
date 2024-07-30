9148184541971f81a49375741d83fe0eab8b1d15
ronin <ronin.lp@gmail.com>
2024-07-30 21:38:20 +0330

    projector done

---
+++ src/bin/projector.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+use clap::Parser;
+use projector::{config::Config, opts::Opts};
+
+fn main() -> Result<()> {
+    let opts: Config = Opts::parse().try_into()?;
+
+    println!("{:?}", opts);
+
+    return Ok(());
+}
